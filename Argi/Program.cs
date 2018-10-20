0dc36cab9a769484ca0ddcf9efabbdf032d119bb
User <User@WIN-R1VEPF5FUNI>
2018-10-20 23:00:27 +0300

    Добавьте файлы проекта.

---
+++ Argi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Argi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+             if (args.Length != 0)
+                 foreach (string s in args)
+                 {                    
+                      str = s ;
+                    
+                }
+
+            Console.WriteLine("write "+str);
+            Console.Read();
+        }
+
+    }
+}
